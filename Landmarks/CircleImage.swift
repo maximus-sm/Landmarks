a8e7518324bc4154f986226c01da6caaf1a4229f
Sunny <sundet.kz@gmail.com>
2024-09-04 17:58:54 +0500

    Initial commit for the Apple's tutorial. Add Views like Text, Image and Map. Combine them with Stacks. Use of modifires: alignment , padding , offset , text style and etc.

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Sundet Mukhtar on 04.09.2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
