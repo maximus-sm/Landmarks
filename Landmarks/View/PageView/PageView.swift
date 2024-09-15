7f9e3b6203c69d3000803fbd1f75b496b2ddaf6a
Sunny <sundet.kz@gmail.com>
2024-09-15 23:09:20 +0500

    Integreating UIKit Subclassing UI protocol and conforming to corresponding methods. Create of a SwiftUI "mirror" class of UIKit UiVC.

---
+++ Landmarks/View/PageView/PageView.swift
@@ -0,0 +1,29 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Sundet Mukhtar on 15.09.2024.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages,currentPage:$currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                           .frame(width: CGFloat(pages.count * 18))
+                           .padding(.trailing)        }
+        .aspectRatio(3 / 2, contentMode: .fit)
+
+        
+    }
+}
+
+
+
+#Preview {
+    PageView(pages: ModelData().featured.map { FeatureCard(landmark: $0) })
+}
